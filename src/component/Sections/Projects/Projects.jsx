9813610c817ceb2d20a65737434f37e73ca0d92f
willyongaks <134366767+Devongaki@users.noreply.github.com>
2023-11-15 13:05:46 +0100

    Update Projects.jsx

--- src/component/Sections/Projects/Projects.jsx
+++ src/component/Sections/Projects/Projects.jsx
@@ -30,13 +30,13 @@ export const Projects = () => {
               </div>
               <div className="project-cta">
                 <div className="github-cta">
-                  <a href="#">
+                  <a href="https://github.com/Devongaki/gamearchieve">
                     <FaGithubSquare />
                     Github
                   </a>
                 </div>
                 <div className="live-cta">
-                  <a href="#">
+                  <a href="https://taupe-cannoli-a5e077.netlify.app">
                     <MdLiveTv />
                     Demo
                   </a>
@@ -66,13 +66,13 @@ export const Projects = () => {
               </div>
               <div className="project-cta">
                 <div className="github-cta">
-                  <a href="#">
+                  <a href="https://github.com/Devongaki/gamearchieve">
                     <FaGithubSquare />
                     Github
                   </a>
                 </div>
                 <div className="live-cta">
-                  <a href="#">
+                  <a href="https://taupe-cannoli-a5e077.netlify.app">
                     <MdLiveTv />
                     Demo
                   </a>
@@ -102,13 +102,13 @@ export const Projects = () => {
               </div>
               <div className="project-cta">
                 <div className="github-cta">
-                  <a href="#">
+                  <a href="https://github.com/Devongaki/gamearchieve">
                     <FaGithubSquare />
                     Github
                   </a>
                 </div>
                 <div className="live-cta">
-                  <a href="#">
+                  <a href="https://taupe-cannoli-a5e077.netlify.app">
                     <MdLiveTv />
                     Demo
                   </a>
@@ -138,13 +138,13 @@ export const Projects = () => {
               </div>
               <div className="project-cta">
                 <div className="github-cta">
-                  <a href="#">
+                  <a href="https://github.com/Devongaki/gamearchieve">
                     <FaGithubSquare />
                     Github
                   </a>
                 </div>
                 <div className="live-cta">
-                  <a href="#">
+                  <a href="https://taupe-cannoli-a5e077.netlify.app">
                     <MdLiveTv />
                     Demo
                   </a>
@@ -152,6 +152,43 @@ export const Projects = () => {
               </div>
             </div>
           </div>
+          <div className="project-items">
+            <div className="project-img">
+              <img src="" alt="" />
+            </div>
+            <div className="project-details">
+              <div className="project-title">
+                <h1>Gamestudios</h1>
+              </div>
+              <div className="project-description">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+                  <br /> <br /> Quod voluptate dolores deserunt reiciendis
+                  aspernatur temporibus tempora natus enim doloribus mollitia.{" "}
+                  <br />
+                  <br /> quos eligendi, id alias pariatur similique ex, minima
+                  facilis repellat?
+                  <br /> quos eligendi, id alias pariatur similique ex, minima
+                  facilis repellat?
+                </p>
+              </div>
+              <div className="project-cta">
+                <div className="github-cta">
+                  <a href="https://github.com/Devongaki/gamearchieve">
+                    <FaGithubSquare />
+                    Github
+                  </a>
+                </div>
+                <div className="live-cta">
+                  <a href="https://taupe-cannoli-a5e077.netlify.app">
+                    <MdLiveTv />
+                    Demo
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          
         </div>
       </div>
     </div>
