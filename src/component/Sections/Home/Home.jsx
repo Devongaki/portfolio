8867e4e0241061425671e4b620c8890494d6e654
willyongaks <134366767+Devongaki@users.noreply.github.com>
2023-11-15 11:43:24 +0100

    project styling

--- src/component/Sections/Home/Home.jsx
+++ src/component/Sections/Home/Home.jsx
@@ -5,7 +5,7 @@ export const Home = () => {
   return (
     <div id='home' className='container'>
       <div className="backgimg-text">
-        <p>William ongaki</p>
+        <p>William Ongaki</p>
         <h1>Frontend <br /> Develope<span>r</span></h1>
       </div>
     </div>
--- src/component/Sections/Projects/Projects.jsx
+++ src/component/Sections/Projects/Projects.jsx
@@ -1,8 +1,159 @@
-import React from 'react';
+import React from "react";
 import "./Projects.css";
+import { FaGithubSquare } from "react-icons/fa";
+import { MdLiveTv } from "react-icons/md";
 
 export const Projects = () => {
   return (
-    <div id='projects'>Projects</div>
-  )
-}
+    <div id="projects">
+      <div className="container">
+        <div className="project-container">
+          <div className="project-items">
+            <div className="project-img">
+              <img src="" alt="" />
+            </div>
+            <div className="project-details">
+              <div className="project-title">
+                <h1>Gamestudios</h1>
+              </div>
+              <div className="project-description">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+                  <br /> <br /> Quod voluptate dolores deserunt reiciendis
+                  aspernatur temporibus tempora natus enim doloribus mollitia.{" "}
+                  <br />
+                  <br /> quos eligendi, id alias pariatur similique ex, minima
+                  facilis repellat?
+                  <br /> quos eligendi, id alias pariatur similique ex, minima
+                  facilis repellat?
+                </p>
+              </div>
+              <div className="project-cta">
+                <div className="github-cta">
+                  <a href="#">
+                    <FaGithubSquare />
+                    Github
+                  </a>
+                </div>
+                <div className="live-cta">
+                  <a href="#">
+                    <MdLiveTv />
+                    Demo
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="project-items">
+            <div className="project-img">
+              <img src="" alt="" />
+            </div>
+            <div className="project-details">
+              <div className="project-title">
+                <h1>Gamestudios</h1>
+              </div>
+              <div className="project-description">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+                  <br /> <br /> Quod voluptate dolores deserunt reiciendis
+                  aspernatur temporibus tempora natus enim doloribus mollitia.{" "}
+                  <br />
+                  <br /> quos eligendi, id alias pariatur similique ex, minima
+                  facilis repellat?
+                  <br /> quos eligendi, id alias pariatur similique ex, minima
+                  facilis repellat?
+                </p>
+              </div>
+              <div className="project-cta">
+                <div className="github-cta">
+                  <a href="#">
+                    <FaGithubSquare />
+                    Github
+                  </a>
+                </div>
+                <div className="live-cta">
+                  <a href="#">
+                    <MdLiveTv />
+                    Demo
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="project-items">
+            <div className="project-img">
+              <img src="" alt="" />
+            </div>
+            <div className="project-details">
+              <div className="project-title">
+                <h1>Gamestudios</h1>
+              </div>
+              <div className="project-description">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+                  <br /> <br /> Quod voluptate dolores deserunt reiciendis
+                  aspernatur temporibus tempora natus enim doloribus mollitia.{" "}
+                  <br />
+                  <br /> quos eligendi, id alias pariatur similique ex, minima
+                  facilis repellat?
+                  <br /> quos eligendi, id alias pariatur similique ex, minima
+                  facilis repellat?
+                </p>
+              </div>
+              <div className="project-cta">
+                <div className="github-cta">
+                  <a href="#">
+                    <FaGithubSquare />
+                    Github
+                  </a>
+                </div>
+                <div className="live-cta">
+                  <a href="#">
+                    <MdLiveTv />
+                    Demo
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="project-items">
+            <div className="project-img">
+              <img src="" alt="" />
+            </div>
+            <div className="project-details">
+              <div className="project-title">
+                <h1>Gamestudios</h1>
+              </div>
+              <div className="project-description">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+                  <br /> <br /> Quod voluptate dolores deserunt reiciendis
+                  aspernatur temporibus tempora natus enim doloribus mollitia.{" "}
+                  <br />
+                  <br /> quos eligendi, id alias pariatur similique ex, minima
+                  facilis repellat?
+                  <br /> quos eligendi, id alias pariatur similique ex, minima
+                  facilis repellat?
+                </p>
+              </div>
+              <div className="project-cta">
+                <div className="github-cta">
+                  <a href="#">
+                    <FaGithubSquare />
+                    Github
+                  </a>
+                </div>
+                <div className="live-cta">
+                  <a href="#">
+                    <MdLiveTv />
+                    Demo
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
